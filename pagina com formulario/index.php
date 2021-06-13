2020fe209b560164ee30ed5fcc5f064f16b9b902
Mateus-desig <40679395+Mateus-desig@users.noreply.github.com>
2021-06-13 19:32:05 -0300

    Primeira versão so projeto

---
+++ pagina com formulario/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moderns</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <section class="paralax p1">
+            <nav id="menu">
+                <h1 title="Nome do site">Nome do site</h1>
+                <ul>
+                    <li><a href="#" title="Contato">Contato</a></li>
+                </ul>
+            </nav>
+
+                <div id="texto-menu">
+                    <div>
+                    <h1>Parece a superfície Lunar</h1>
+                    <p>É fato! As ruas em sua maioria estão em péssimas condições, sejam em bairros onde o tráfego de pessoas é grande ou em bairros onde tem um tráfego menor, enfim, ruas nessas condições geram grandes problemas.</p>
+                    </div>
+                </div>
+
+        </section>
+    </header>
+
+    <h1 id="titulo">BLOG: </h1>
+
+    <div id="box-form">
+        <form id="form" action="#" method="post">
+            <label for="nome">
+                <input type="name" name="nome" id="nome" placeholder="Nome" required>
+            </label>
+            <label for="sobre-nome">
+                <input type="name" name="sobre-nome" id="sobre-nome" placeholder="Sobre nome" required>
+            </label>
+            <label for="email">
+                <input type="email" name="email" id="email" placeholder="Email" required>
+            </label>
+            <label for="tel">
+                <p>Celular:</p>
+                <input type="tel" name="tel" id="tel" placeholder="(99) 999-999-999" required>
+            </label>
+
+            <input type="submit" value="Enviar" id="custon-buttom">
+        </form>
+    </div>
+
+
+    <div class="box-items">
+        <h1>Posts:</h1>
+
+        <div class="item-play">
+            <a href="#">
+                <img src="https://a-static.mlcdn.com.br/618x463/pc-gamer-completo-g-fire-htg-420-amd-3000g-3-5ghz-8gb-radeon-rx-vega-2gb-1tb-monitor-18/gfire/htg-420/e4e4c163a0d0793710568c664ae7a0ac.jpg" >
+                <p>Pc Gamer Completo</p>
+            </a>
+        </div>
+
+        <div class="item-play">
+            <a href="#">
+                <img src="https://a-static.mlcdn.com.br/618x463/pc-gamer-completo-g-fire-htg-420-amd-3000g-3-5ghz-8gb-radeon-rx-vega-2gb-1tb-monitor-18/gfire/htg-420/e4e4c163a0d0793710568c664ae7a0ac.jpg" >
+                <p>Pc Gamer Completo</p>
+            </a>
+        </div>
+
+        <div class="item-play">
+            <a href="#">
+                <img src="https://a-static.mlcdn.com.br/618x463/pc-gamer-completo-g-fire-htg-420-amd-3000g-3-5ghz-8gb-radeon-rx-vega-2gb-1tb-monitor-18/gfire/htg-420/e4e4c163a0d0793710568c664ae7a0ac.jpg" >
+                <p>Pc Gamer Completo</p>
+            </a>
+        </div>
+
+        <div class="item-play">
+            <a href="#">
+                <img src="https://a-static.mlcdn.com.br/618x463/pc-gamer-completo-g-fire-htg-420-amd-3000g-3-5ghz-8gb-radeon-rx-vega-2gb-1tb-monitor-18/gfire/htg-420/e4e4c163a0d0793710568c664ae7a0ac.jpg" >
+                <p>Pc Gamer Completo</p>
+            </a>
+        </div>
+
+    </div>
+
+
+    <footer id="footer">
+        <h1 id="web-name" title="Nome do site"><a href="#">Nome do site</a></h1>
+        <div class="contato"><a href="#" title="Contato">Contato</a></div>
+    </footer>
+</body>
+</html>
